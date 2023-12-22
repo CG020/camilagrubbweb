f36995fe077fb8139e701f6d9df0f1320297e14a
CG020 <gmills020@gmail.com>
2023-12-21 18:49:06 -0700

    profile picture added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 function Header() {
   return (
     <div className="header">
+      <div className="profile-image-container">
+        <img src="src/assets/originalImage.jpg" alt="Profile" className="profile-image"/>
+      </div>
       <h1>Camila Grubb</h1>
       <button>Contact Me</button>
     </div>
