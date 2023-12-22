9399b5bbdd4026dd5e6a124e8ba32129ab00b18d
CG020 <gmills020@gmail.com>
2023-12-21 19:21:22 -0700

    changed design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,8 @@
 function Header() {
   return (
     <div className="header">
-      <div className="profile-image-container">
-        <img src="src/assets/originalImage.jpg" alt="Profile" className="profile-image"/>
-      </div>
-      <h1>Camila Grubb</h1>
-      <button>Contact Me</button>
+      <h1><i>Hello, I'm<br></br><b>Camila Grubb</b></i></h1>
+      <button><i>Contact Me</i></button>
     </div>
   );
 }
