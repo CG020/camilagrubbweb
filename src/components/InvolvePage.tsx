0957163c9c5816bb7ba7571f18fb0c9c39e5f66a
CG020 <gmills020@gmail.com>
2024-03-02 01:37:03 -0700

    updates involvement page

--- src/components/InvolvePage.tsx
+++ src/components/InvolvePage.tsx
@@ -5,15 +5,24 @@ import NavBar from "./NavBar";
 
 const InvolvePage = () => {
     const rectButton = {
-        width: '85vw',
-        height: 'auto',
+        width: '90vw',
+        height: '100px',
         padding: '15px',
         margin: '30px',
+        marginBottom: '5px',
         backgroundColor: '#333',
         display: 'flex',
         flexDirection: 'column',
         borderRadius: '8px',
         boxSizing: 'border-box',
+        fontFamily: 'monospace, sans-serif',
+        textAlign: 'left',
+    };
+
+    const rectContent = {
+        width: '85vw',
+        height: 'auto',
+        marginLeft: '35px',
         fontFamily: 'monospace, sans-serif',
     };
 
@@ -21,13 +30,32 @@ const InvolvePage = () => {
         <div>
             <NavBar message={'documenting my involvement'} tabs={['university', 'volunteer']}/>
             <p className="d-inline-flex gap-1">
-                <a className="btn btn-primary" style={rectButton} data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                    Link with href
+                <a className="btn btn-primary" style={rectButton} data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    Design Labs -- Working with APEX Applied Technologies --
                 </a>
             </p>
-            <div className="collapse" id="collapseExample">
+            <div className="collapse" style={rectContent} id="collapse1">
                 <div className="card card-body">
-                    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                    At the University of Arizona Design Labs, I got the oppurtunity to partner with several students
+                    from multiple disciplines at the university. APEX Applied Technologues helps marginalized groups and 
+                    small communities to improve quality of life environmentally and economically. We put together a 
+                    team to work with APEX Applied Technologies on a toolkit for clients interested in the economic services.
+                    This toolkit offers comprehensive guidance on the intricate processes involved in federal grant writing and 
+                    management. This initiative sought to empower clients with the knowledge and tools necessary to secure funding 
+                    that could significantly improve their communities' quality of life. Through this collaborative effort, we were 
+                    able to contribute to APEX's mission of fostering sustainable development and economic growth.
+                </div>
+            </div>
+
+            <p className="d-inline-flex gap-1">
+                <a className="btn btn-primary" style={rectButton} data-bs-toggle="collapse" href="#collapse2" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    Angel Heart Pajama Project Volunteering
+                </a>
+            </p>
+            <div className="collapse" style={rectContent} id="collapse2">
+                <div className="card card-body">
+                    Angel Heart is a Southern Arizona Based volunteer group that helps children have access to a warm and welcoming 
+                    community and 
                 </div>
             </div>
             <Footer></Footer>
